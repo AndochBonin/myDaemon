173c34edb4d1d5705b553faa89f43fff30796e04
Andoch Bonin <66486863+AndochBonin@users.noreply.github.com>
2025-06-19 18:50:43 +0000

    write add and delete process functions for scheduler

--- process/process.go
+++ process/process.go
@@ -1,9 +1,16 @@
 package process
 
 import (
+	"sync"
 	"time"
-
+	"slices"
 	"github.com/AndochBonin/myDaemon/program"
+)
+
+var (
+	scheduler *Scheduler
+	once sync.Once
+	SchedulerError error
 )
 
 type Process struct {
@@ -13,6 +20,51 @@ type Process struct {
 	IsRecurring bool
 }
 
-type Schedule []Process
+type Scheduler struct {
+	CurrentProcess *Process
+	Schedule []Process
+}
 
-// Define 
+func GetScheduler() *Scheduler {
+	createScheduler := func() {
+		scheduler = &Scheduler{}
+	}
+	once.Do(createScheduler)
+	return scheduler
+}
+
+func (scheduler *Scheduler) AddProcess(process Process) error {
+	insertIdx := 0
+
+	for insertIdx < len(scheduler.Schedule) {
+		scheduleProcess := scheduler.Schedule[insertIdx]
+
+		if process.StartTime.Equal(scheduleProcess.StartTime) {
+			return SchedulerError
+		} else if  process.StartTime.After(scheduleProcess.StartTime) {
+			insertIdx++
+		} else {
+			break
+		}
+	}
+
+	if insertIdx < len(scheduler.Schedule) {
+		nextProcessStartTime := scheduler.Schedule[insertIdx].StartTime
+		processEndTime := process.StartTime.Add(process.DurationNanoseconds)
+
+		if processEndTime.After(nextProcessStartTime) {
+			return SchedulerError
+		}
+	}
+
+	scheduler.Schedule = slices.Insert(scheduler.Schedule, insertIdx, process)
+	return nil
+}
+
+func (scheduler *Scheduler) RemoveProcess(processID int) error {
+	if processID < 0 || processID >= len(scheduler.Schedule) {
+		return SchedulerError
+	}
+	scheduler.Schedule = slices.Delete(scheduler.Schedule, processID, processID + 1)
+	return nil
+}
