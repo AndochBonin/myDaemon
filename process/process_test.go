b56742e309663b008cd43f232c280400aad37663
Andoch Bonin <66486863+AndochBonin@users.noreply.github.com>
2025-06-22 16:52:04 +0000

    write tests for RemoveProcess function

--- process/process_test.go
+++ process/process_test.go
@@ -23,7 +23,7 @@ func TestGetScheduler(t *testing.T) {
 		go func() {
 			defer wg.Done()
 			if GetScheduler() != scheduler {
-				t.Error("GetScheduler created a new scheduler in a separater goroutine")
+				t.Error("GetScheduler created a new scheduler in a separate goroutine")
 			}
 		}()
 	}
@@ -104,13 +104,94 @@ func TestAddProcess(t *testing.T) {
 	})
 }
 
-func TestDeleteProcess(t *testing.T) {
-	// test deleting a process by valid index
-	// test deleting a recurring process and keeping recurrence (re-added with +24h offset)
-	// test deleting a recurring process and ending recurrence (not re-added)
-	// test deleting with invalid index (negative or out of bounds)
-	// test recurrence is re-added in correct sorted position
-	// test recurrence fails if it overlaps with an existing process
+func TestRemoveProcess(t *testing.T) {
+	scheduler := GetScheduler()
+	scheduler.Schedule = nil
+
+	t.Run("remove invalid index", func(t *testing.T) {
+		scheduler.Schedule = nil
+		process := Process{}
+		err := scheduler.AddProcess(process)
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+		err = scheduler.RemoveProcess(-1, true)
+		if err != ErrSchedule {
+			t.Errorf("Expected %v got %v instead", ErrSchedule, err)
+		}
+		err = scheduler.RemoveProcess(1, true)
+		if err != ErrSchedule {
+			t.Errorf("Expected %v got %v instead", ErrSchedule, err)
+		}
+		checkSchedule(t, scheduler.Schedule, []Process{process})
+	})
+
+	t.Run("remove valid index, end recurrence", func(t *testing.T) {
+		scheduler.Schedule = nil
+		addErr := scheduler.AddProcess(Process{})
+		removeErr := scheduler.RemoveProcess(0, true)
+		if addErr != nil || removeErr != nil {
+			t.Errorf("Unexpected errors: %v, %v", addErr, removeErr)
+		}
+		checkSchedule(t, scheduler.Schedule, []Process{})
+	})
+
+	t.Run("remove valid index, keep recurrence", func(t *testing.T) {
+		scheduler.Schedule = nil
+		process := newMockProcess(t, "recurring", time.Second, time.Hour, true)
+		addErr := scheduler.AddProcess(process)
+		removeErr := scheduler.RemoveProcess(0, false)
+		if addErr != nil || removeErr != nil {
+			t.Errorf("Unexpected errors: %v, %v", addErr, removeErr)
+		}
+		process.StartTime = process.StartTime.Add(time.Hour * 24)
+		process.EndTime = process.EndTime.Add(time.Hour * 24)
+		checkSchedule(t, scheduler.Schedule, []Process{process})
+	})
+
+	t.Run("process with recurrence reinserted in correct order", func(t *testing.T) {
+		scheduler.Schedule = nil
+		testProcess := newMockProcess(t, "recurring", time.Second, time.Hour, true)
+		beforeTestProcess := newMockProcess(t, "earlier", -time.Hour, time.Minute * 30, false)
+		afterTestProcess := newMockProcess(t, "later", time.Hour * 2, time.Minute * 30, false)
+		afterRecurredProcess := newMockProcess(t, "later", time.Hour * 27, time.Minute * 30, false)
+
+		scheduler.AddProcess(testProcess)
+		scheduler.AddProcess(beforeTestProcess)
+		scheduler.AddProcess(afterTestProcess)
+		scheduler.AddProcess(afterRecurredProcess)
+		// now schedule should look like: {earlier, recurring, later, muchlater}
+		removeErr := scheduler.RemoveProcess(1, false)
+		if removeErr != nil {
+			t.Errorf("Unexpected error: %v", removeErr)
+		}
+		testProcess.StartTime = testProcess.StartTime.Add(time.Hour * 24)
+		testProcess.EndTime = testProcess.EndTime.Add(time.Hour * 24)
+
+		// recurring should slot between later and much later
+		expected := []Process{beforeTestProcess, afterTestProcess, testProcess, afterRecurredProcess}
+		checkSchedule(t, scheduler.Schedule, expected)
+	})
+
+	t.Run("recurring process reinsertion fails when overlapping with existing process", func(t *testing.T) {
+		scheduler.Schedule = nil
+		testProcess := newMockProcess(t, "recurring", time.Second, time.Hour, true)
+		beforeTestProcess := newMockProcess(t, "earlier", -time.Hour, time.Minute * 30, false)
+		afterTestProcess := newMockProcess(t, "later", time.Hour * 2, time.Minute * 30, false)
+		duringRecurredProcess := newMockProcess(t, "later", time.Hour * 24, time.Hour, false)
+
+		scheduler.AddProcess(testProcess)
+		scheduler.AddProcess(beforeTestProcess)
+		scheduler.AddProcess(afterTestProcess)
+		scheduler.AddProcess(duringRecurredProcess)
+		// now schedule should look like: {earlier, recurring, later, muchlater}
+		removeErr := scheduler.RemoveProcess(1, false)
+		if removeErr != ErrSchedule {
+			t.Errorf("Expected: %v got %v instead", ErrSchedule, removeErr)
+		}
+		expected := []Process{beforeTestProcess, afterTestProcess, duringRecurredProcess}
+		checkSchedule(t, scheduler.Schedule, expected)
+	})
 }
 
 func TestRunScheduler(t *testing.T) {
