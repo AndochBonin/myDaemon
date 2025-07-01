83f4219a2bb3791fa53a8a63f89e1bad19e3ccde
Andoch Bonin <66486863+AndochBonin@users.noreply.github.com>
2025-07-01 00:00:19 +0000

    create go routine to update schedule

--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"fmt"
-	//"github.com/AndochBonin/myDaemon/process"
+	//"time"
+
+	"github.com/AndochBonin/myDaemon/process"
+
 	//"github.com/AndochBonin/myDaemon/program"
 	"github.com/AndochBonin/myDaemon/tui"
 )
 
 //var programListFile string = "./program/programList.json"
-
+func UpdateSchedule(scheduler *process.Scheduler) {
+	for {
+		scheduler.UpdateSchedule()
+		//time.Sleep(time.Second)
+	}
+}
+ 
 func main() {
-
+	scheduler := process.GetScheduler()
+	go UpdateSchedule(scheduler)
 	err := tui.Run()
-
 	if err != nil {
 		fmt.Println("welp oops")
 	}
